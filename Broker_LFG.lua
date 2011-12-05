154b3245d25edac89e435f2ddd167e0f9386ba34
Phanx <addons@phanx.net>
2011-12-05 09:17:03 +0000

    - Add support for different LFG/LFR icons in WoW 4.3 - Add ptBR translations

--- Broker_LFG.lua
+++ Broker_LFG.lua
@@ -30,9 +30,9 @@ elseif locale == "frFR" then
 	CLICK_TOGGLE_LFD = "Cliquer pour afficher/fermer cadre des donjons."
 	CLICK_TOGGLE_LFR = "Clic droit pour afficher/fermer la recherche de raid."
 elseif locale == "ptBR" then
---	LFG = ""
---	CLICK_TOGGLE_LFD = ""
---	CLICK_TOGGLE_LFR = ""
+	LFG = ""
+	CLICK_TOGGLE_LFD = "Clique para mostrar/ocultar o painel localizador de masmorras."
+	CLICK_TOGGLE_LFR = "Clique com o botão direito do mouse para mostrar/ocultar o painel localizador de raides"
 elseif locale == "ruRU" then
 	LFG = "ЛФГ"
 	CLICK_TOGGLE_LFD = "Щелкните, чтобы открыть окно поиска подземелий."
@@ -53,18 +53,24 @@ end
 
 ------------------------------------------------------------------------
 
-local iconCoords = setmetatable({}, { __index = function(t, i)
-	if type(i) ~= "number" then i = 1 end
+for queueType, t in pairs(LFG_EYE_TEXTURES) do
+	local cols = floor(t.width / t.iconSize)
+	local colWidth = t.iconSize / t.width
+	local rowHeight = t.iconSize / t.height
 
-	local L = mod(i - 1, 8) * 0.125
-	local R = L + 0.125
-	local B = ceil(i / 8) * 0.25
-	local T = B - 0.25
+	local iconCoords = { }
+	for i = 1, t.frames do
+		local L = mod(i - 1, cols) * colWidth
+		local R = L + colWidth
+		local T = ceil(i / cols) * rowHeight
+		local B = T - rowHeight
 
-	local c = { L, R, T, B }
-	t[i] = c
-	return c
-end })
+		iconCoords[i] = { L, R, B, T }
+	end
+	t.iconCoords = iconCoords
+end
+
+------------------------------------------------------------------------
 
 local function GetScreenHalf()
 	local _, y = GetCursorPosition()
@@ -83,7 +89,7 @@ BrokerLFG.feed = LibStub("LibDataBroker-1.1"):NewDataObject("LFG", {
 	type = "launcher",
 	icon = "Interface\\LFGFrame\\LFG-Eye",
 	label = LFG,
-	iconCoords = iconCoords[1],
+	iconCoords = LFG_EYE_TEXTURES.default.iconCoords[1],
 	OnClick = function(self, button)
 		local mode = GetLFGMode()
 		if mode then
@@ -94,50 +100,55 @@ BrokerLFG.feed = LibStub("LibDataBroker-1.1"):NewDataObject("LFG", {
 				MiniMapLFGFrameDropDown.relativePoint = screenHalf == "TOP" and "BOTTOMLEFT" or "TOPLEFT"
 				ToggleDropDownMenu(1, nil, MiniMapLFGFrameDropDown, self:GetName() or self, 0, 0)
 			elseif mode == "proposal" then
-				if not LFDDungeonReadyPopup:IsShown() then
+				if not LFGDungeonReadyPopup:IsShown() then
 					PlaySound("igCharacterInfoTab")
-					StaticPopupSpecial_Show(LFDDungeonReadyPopup)
+					StaticPopupSpecial_Show(LFDGungeonReadyPopup)
 				end
 			elseif mode == "queued" or mode == "rolecheck" or mode == "suspended" then
-				ToggleLFDParentFrame()
+				if select(8, GetLFGInfoServer()) then
+					ToggleRaidFrame(1)
+				else
+					ToggleLFDParentFrame()
+				end
 			elseif mode == "listed" then
 				ToggleFriendsFrame(4)
 			end
 		elseif button == "RightButton" then
-			ToggleFriendsFrame(4)
+			ToggleRaidFrame(1)
 		else
 			ToggleLFDParentFrame()
 		end
 	end,
 	OnEnter = function(self)
-		local mode = GetLFGMode()
+		local mode, submode = GetLFGMode()
+		local queueType = GetLFGModeType()
 		if mode == "queued" then
 			local screenHalf = GetScreenHalf()
-			LFDSearchStatus:SetParent(UIParent)
-			LFDSearchStatus:SetClampedToScreen(true)
-			LFDSearchStatus:ClearAllPoints()
-			LFDSearchStatus:SetPoint(screenHalf, self, screenHalf == "TOP" and "BOTTOM" or "TOP")
-			LFDSearchStatus:Show()
+			LFGSearchStatus:SetParent(UIParent)
+			LFGSearchStatus:SetClampedToScreen(true)
+			LFGSearchStatus:ClearAllPoints()
+			LFGSearchStatus:SetPoint(screenHalf, self, screenHalf == "TOP" and "BOTTOM" or "TOP")
+			LFGSearchStatus:Show()
 		elseif mode == "proposal" then
 			GameTooltip:SetOwner(self, GetScreenHalf() == "TOP" and "ANCHOR_BOTTOM" or "ANCHOR_TOP")
-			GameTooltip:SetText(LOOKING_FOR_DUNGEON)
+			GameTooltip:SetText(queueType == "raid" and RAID_FINDER or LOOKING_FOR_DUNGEON)
 			GameTooltip:AddLine(DUNGEON_GROUP_FOUND_TOOLTIP, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, 1)
 			GameTooltip:AddLine(" ")
 			GameTooltip:AddLine(CLICK_HERE_FOR_MORE_INFO, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, 1)
 			GameTooltip:Show()
 		elseif mode == "rolecheck" then
 			GameTooltip:SetOwner(self, GetScreenHalf() == "TOP" and "ANCHOR_BOTTOM" or "ANCHOR_TOP")
-			GameTooltip:SetText(LOOKING_FOR_DUNGEON)
+			GameTooltip:SetText(queueType == "raid" and RAID_FINDER or LOOKING_FOR_DUNGEON)
 			GameTooltip:AddLine(ROLE_CHECK_IN_PROGRESS_TOOLTIP, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, 1)
 			GameTooltip:Show()
 		elseif mode == "listed" then
 			GameTooltip:SetOwner(self, GetScreenHalf() == "TOP" and "ANCHOR_BOTTOM" or "ANCHOR_TOP")
-			GameTooltip:SetText(LOOKING_FOR_RAID)
+			GameTooltip:SetText(queueType == "raid" and RAID_FINDER or LOOKING_FOR_DUNGEON)
 			GameTooltip:AddLine(YOU_ARE_LISTED_IN_LFR, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, 1)
 			GameTooltip:Show()
 		elseif mode == "lfgparty" then
 			GameTooltip:SetOwner(self, GetScreenHalf() == "TOP" and "ANCHOR_BOTTOM" or "ANCHOR_TOP")
-			GameTooltip:SetText(LOOKING_FOR_DUNGEON)
+			GameTooltip:SetText(queueType == "raid" and RAID_FINDER or LOOKING_FOR_DUNGEON)
 			GameTooltip:AddLine(YOU_ARE_IN_DUNGEON_GROUP, NORMAL_FONT_COLOR.r, NORMAL_FONT_COLOR.g, NORMAL_FONT_COLOR.b, 1)
 
 			local dungeon = GetPartyLFGID()
@@ -158,7 +169,7 @@ BrokerLFG.feed = LibStub("LibDataBroker-1.1"):NewDataObject("LFG", {
 			GameTooltip:Show()
 		elseif mode == "suspended" then
 			GameTooltip:SetOwner(self, GetScreenHalf() == "TOP" and "ANCHOR_BOTTOM" or "ANCHOR_TOP")
-			GameTooltip:SetText(LOOKING_FOR_DUNGEON)
+			GameTooltip:SetText(queueType == "raid" and RAID_FINDER or LOOKING_FOR_DUNGEON)
 			GameTooltip:AddLine(IN_LFG_QUEUE_BUT_SUSPENDED, nil, nil, nil, 1)
 			GameTooltip:Show()
 		else
@@ -171,22 +182,26 @@ BrokerLFG.feed = LibStub("LibDataBroker-1.1"):NewDataObject("LFG", {
 	end,
 	OnLeave = function(self)
 		GameTooltip:Hide()
-		local f = LFGSearchStatus or LFDSearchStatus
-		f:Hide()
+		LFGSearchStatus:Hide()
 	end,
 })
 
 ------------------------------------------------------------------------
 
+local currentQueueType
 local currentFrame = 1
+
+local currentIcon = LFG_EYE_TEXTURES.default
+local iconCoords = currentIcon.iconCoords
+local updateDelay = currentIcon.delay
 
 local counter = 0
 local function UpdateIconCoords(self, elapsed)
 	counter = counter + elapsed
-	if counter > 0.1 then
-		if currentFrame > 29 then
+	if counter > updateDelay then
+		if currentFrame > currentIcon.frames then
 			currentFrame = 1
-		end
+		end		
 		self.feed.iconCoords = iconCoords[currentFrame] or iconCoords[1]
 		currentFrame = currentFrame + 1
 		counter = 0
@@ -195,12 +210,28 @@ end
 
 BrokerLFG:SetScript("OnEvent", function(self, event)
 	local mode = GetLFGMode()
-	if mode == "queued" or mode == "listed" or mode == "rolecheck" then
+	local queueType
+	if mode then
+		if mode == "queued" and not GetLFGQueueStats() then
+			queueType = "unknown"
+		else
+			queueType = GetLFGModeType()
+		end
+	end
+	if queueType ~= currentQueueType then
+		currentQueueType = queueType
+		currentIcon = LFG_EYE_TEXTURES[queueType] or LFG_EYE_TEXTURES.default
+		iconCoords = currentIcon.iconCoords
+		updateDelay = currentIcon.delay
+		self.feed.icon = currentIcon.file
+	end
+	if mode == "queued" or mode == "listed" or mode == "rolecheck" or mode == "suspended" then
+		currentFrame = 1
 		self:SetScript("OnUpdate", UpdateIconCoords)
 	else
 		self:SetScript("OnUpdate", nil)
-		self.feed.iconCoords = iconCoords[1]
 		currentFrame = 1
+		self.feed.iconCoords = iconCoords[1]
 	end
 end)
 
