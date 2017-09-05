b6df8ffd95d74e6aa9e53014d056cf12ef3446ef
Phanx <addons@phanx.net>
2017-09-04 19:59:25 -0700

    Use the Blizzard constants for sound file IDs

--- Addon.lua
+++ Addon.lua
@@ -156,7 +156,7 @@ addon.feed = LibStub("LibDataBroker-1.1"):NewDataObject("LFG", {
 	end,
 	OnClick = function(self, button)
 		if button == "RightButton" and GetQueueInfo() and not IsShiftKeyDown() then
-			PlaySound(850)
+			PlaySound(SOUNDKIT.IG_MAINMENU_OPEN)
 			local screenHalf = GetScreenHalf()
 			QueueStatusMinimapButtonDropDown.point = screenHalf == "TOP" and "TOPLEFT" or "BOTTOMLEFT"
 			QueueStatusMinimapButtonDropDown.relativePoint = screenHalf == "TOP" and "BOTTOMLEFT" or "TOPLEFT"
